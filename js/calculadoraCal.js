aab48f6b06f085e3c92ced6de827edeaf305be8f
Samuel Romero <sromerom@esliceu.net>
2020-01-16 12:53:15 +0100

    Implementat tot, nomes quedaria fer petites millores voluntaries

--- js/calculadoraCal.js
+++ js/calculadoraCal.js
@@ -1,5 +1,6 @@
 const buttonCalc = document.querySelector("#calc");
 
+//Button per calcular les calories necessaries
 buttonCalc.addEventListener("click", function() {
     let sexe;
     if (document.querySelector('#rad1').checked) {
@@ -12,18 +13,16 @@ buttonCalc.addEventListener("click", function() {
     const altura = document.querySelector("#altura").value;
     const edat = document.querySelector("#edat").value;
     const activitat = document.querySelector("#activitat").value;
-
-    console.log(sexe);
-    console.log(pes)
-    console.log(altura);
-    console.log(edat);
-    console.log(activitat)
-
     const tmb = calculTMB(sexe, pes, altura, edat);
     const calNecessaries = tmb * activitat;
+
+    if (pes) {
+        console.log("Estan tots!!!!");
+    }
     document.querySelector("#resultat").innerHTML = calNecessaries + " KCal";
 })
 
+//Funcio que ens calcula la tassa metabòlica basal (TMB)
 function calculTMB(sexe, pes, altura, edat) {
     if (sexe === "home") {
         return (10 * pes) + (6.25 * altura) - (5 * edat) + 5;
@@ -32,23 +31,20 @@ function calculTMB(sexe, pes, altura, edat) {
     }
 }
 
+/*#### Drag and Drop ####*/
 function startDrag(ev) {
-    console.log("Comença el drag")
     ev.dataTransfer.setData("idSelec", ev.target.id);
 }
 
 function endDrag(ev) {
-    console.log("Termina el drag")
     ev.dataTransfer.clearData();
 }
 
 function zoneDrop(ev) {
-    console.log("zona a on es pot fer drop")
     ev.preventDefault();
 }
 
 function drop(ev) {
-    console.log("Drop!")
     ev.preventDefault();
 
     const myNode = document.querySelector("#destiDrag");
@@ -90,28 +86,26 @@ function drop(ev) {
 
     }
 
-    console.log(id)
 }
 
-//Cream la base de dades local
-
+/*#### IndexedDB ####*/
+//Cream la base de dades local a on es guardaran tots els aliments introduits
+const req = indexedDB.deleteDatabase("Aliments");
 var request = window.indexedDB.open("Aliments", 1);
 
 request.onupgradeneeded = async function(event) {
-    var db = event.target.result;
+    const db = event.target.result;
     let objectStore = db.createObjectStore("aliment", { autoIncrement: true });
 
 };
 
 
-
-// Put variables in global scope to make them available to the browser console.
 let classifier;
 let resultsP;
-var video;
+let video;
 
 //Cream l'objecte constraint que fara servir API de la camara, mediaDevices
-var constraints = window.constraints = {
+const constraints = window.constraints = {
     audio: false,
     video: {
         width: { min: 640 },
@@ -119,20 +113,14 @@ var constraints = window.constraints = {
     }
 };
 
-/* md5 */
+/*#### md5 ####*/
+//Reconeixement d'aliments
 function setup() {
     noCanvas();
     // Create a camera input
     video = document.querySelector('video');
     navigator.mediaDevices.getUserMedia(constraints)
         .then(function(stream) {
-            var videoTracks = stream.getVideoTracks();
-            console.log('Got stream with constraints:', constraints);
-            console.log('Using video device: ' + videoTracks[0].label);
-
-            stream.onremovetrack = function() {
-                console.log('Stream ended');
-            };
             window.stream = stream; // make variable available to browser console
             video.srcObject = stream;
         })
@@ -151,20 +139,48 @@ function classifyVideo() {
     classifier.classify(gotResult);
 }
 
-// When we get a result
+
 function gotResult(err, results) {
-    console.log(results)
-        // The results are in an array ordered by confidence.
-        //resultsP.html(results[0].label + ' ' + nf(results[0].confidence, 0, 2));
     document.querySelector("#reconeixement").innerHTML = results[0].label + ' ' + nf(results[0].confidence, 0, 2);
+    //Si la confiança es major al 0.70, començarem el proces d'introducció a la base de dades
     if (results[0].confidence >= 0.70) {
-        console.log("Se añade: " + results[0].label);
         addData(results[0].label);
     }
     classifyVideo();
 }
 
-//Funcio que afegeix un aliment a la base de dades. Previament es comprova que aquest aliment no estigui inserit d'abans.
+const calculaDieta = document.querySelector("#calculaDieta");
+const eliminarDieta = document.querySelector("#elimina");
+
+calculaDieta.addEventListener("click", function() {
+    const db = request.result;
+
+    const transaction = db.transaction("aliment", "readwrite");
+    const objectStore = transaction.objectStore("aliment");
+    let sumaCal = 0;
+    //Abans d'afegir el nou aliment, s'ha de veure si l'aliment es troba en la bbdd o no
+    const aliments = objectStore.getAll();
+
+    aliments.onsuccess = function(e) {
+        const totsAliments = aliments.result;
+
+        //De cada aliment de la base de dades analitzam les seves calories
+        totsAliments.forEach(async function(aliment) {
+            let alimentWithCal = await getCalories(aliment.nom);
+            sumaCal = sumaCal + alimentWithCal.hints[0].food.nutrients.ENERC_KCAL;
+            document.querySelector("#dieta").innerHTML = sumaCal + " KCal";
+        })
+    }
+})
+
+//Boto que ens permet esborrar tots els ingredients de la base de dades i començar un altre dieta de nou.
+eliminarDieta.addEventListener("click", function() {
+    const req = indexedDB.deleteDatabase("Aliments");
+    window.location.href = "calculadorCal.html";
+})
+
+/*#### Funcions ####*/
+//Funcio que afegeix un aliment a la base de dades. Es comprova que aquest aliment no ha estat inserit previament.
 function addData(alimentNou) {
     const db = request.result;
 
@@ -185,10 +201,8 @@ function addData(alimentNou) {
             }
         })
 
-        console.log(existeix)
-            //Si no existeix, es comença a introduir l'aliment a la base de dades
+        //Si no existeix, es comença a introduir l'aliment a la base de dades
         if (!existeix) {
-            console.log("No existeix!!, Introduint ingredient")
             const transaction = db.transaction("aliment", "readwrite");
             const objectStore = transaction.objectStore("aliment");
 
@@ -198,12 +212,11 @@ function addData(alimentNou) {
             objectStore.add(newAliment);
             document.querySelector("#totIngredients").innerHTML += "<p>" + alimentNou + "</p>";
 
-        } else {
-            console.log("L'ingredient existeix...")
         }
     }
 }
 
+//Funcio que ens permet saber quantes calories te un ingredient en concret
 async function getCalories(nameIngr) {
     const app_id = "32f2df88";
     const app_key = "53d0545fb61ad0cc3a9ea0af076a5e05";
@@ -213,45 +226,3 @@ async function getCalories(nameIngr) {
     return ingredients;
 
 }
-
-const calculaDieta = document.querySelector("#calculaDieta");
-const eliminarDieta = document.querySelector("#elimina");
-
-calculaDieta.addEventListener("click", function() {
-    const db = request.result;
-
-    const transaction = db.transaction("aliment", "readwrite");
-    const objectStore = transaction.objectStore("aliment");
-    var sumaCal = 0;
-    let arraysCalories = [];
-    //Abans d'afegir el nou aliment, s'ha de veure si l'aliment es troba en la bbdd o no
-    const aliments = objectStore.getAll();
-
-    aliments.onsuccess = function(e) {
-        const totsAliments = aliments.result;
-
-        totsAliments.forEach(async function(aliment) {
-            //console.log(aliment)
-            let alimentWithCal = await getCalories(aliment.nom);
-            sumaCal = sumaCal + alimentWithCal.hints[0].food.nutrients.ENERC_KCAL;
-            console.log(sumaCal)
-            document.querySelector("#dieta").innerHTML = sumaCal + " KCal";
-        })
-    }
-})
-
-
-eliminarDieta.addEventListener("click", function() {
-    console.log("Eliminando!!!!")
-    var req = indexedDB.deleteDatabase("Aliments");
-    req.onsuccess = function() {
-        console.log("Deleted database successfully");
-    };
-    req.onerror = function() {
-        console.log("Couldn't delete database");
-    };
-    req.onblocked = function() {
-        console.log("Couldn't delete database due to the operation being blocked");
-    };
-    window.location.href = "calculadorCal.html";
-})
